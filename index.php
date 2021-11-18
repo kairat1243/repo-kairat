e35100ea834188d775411b3168b00440dafb91b0
kairat1243 <92501551+kairat1243@users.noreply.github.com>
2021-11-18 12:53:09 +0600

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                <p>home address: Archa Beshik 6/72</p>
                <p>my social media</p>
     <div>Instagram: <a href="https://www.instagram.com/evgenii_ponasenkov/" target="_blank">@evgenii_ponasenkov</a>></a></div>   
-    <div>YouTube: <a href="https://youtu.be/ZudsBqbtws8" target="_blank">Log in</a>></a></div>    
+    <div>YouTube: <a href="https://youtu.be/ZudsBqbtws8" target="_blank">Log in</a>></a></div> 
     <div>STEAM: <a href="https://steamcommunity.com/id/Kairat2018/"Log in</a>></a></div>  
 
            </h2></th>
